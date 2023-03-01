0d43cb7bffb0c5b184be2ddedae5949b4bcf9f83
aymane4code <aymaneassabiri259@gmail.com>
2023-03-01 09:43:06 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "sawma3at hassan",
+        location: "morocco",
+        googleMapsUrl: "https://goo.gl/maps/4SCzQa3Yri7bVSFz7",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Hassan Tower or Tour Hassan is the minaret of an incomplete mosque in Rabat, Morocco. It was commissioned by Abu Yusuf Yaqub al-Mansur, the third Caliph of the Almohad Caliphate, near the end of the 12th century.",
+        imageUrl: "https://lh3.googleusercontent.com/p/AF1QipNXcphuhEdcU0954OqNxRggQh3wWt6ZDl2vJYt8=s680-w680-h510"
+    }
+]
